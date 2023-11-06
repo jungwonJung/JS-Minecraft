b4f3ca93d70683be576f7f5830f669e60c109811
jungwonJung <wjdwjd1501@gmail.com>
2023-11-06 14:48:34 +0100

    player and keyboard mapping

---
+++ src/images/textures.js
@@ -0,0 +1,18 @@
+import { dirtImg, grassImg, glassImg, woodImg, logImg } from "./images";
+import { TextureLoader } from "three";
+
+const dirtTexture = new TextureLoader().load(dirtImg);
+const logTexture = new TextureLoader().load(logImg);
+const grassTexture = new TextureLoader().load(grassImg);
+const glassTexture = new TextureLoader().load(glassImg);
+const woodTexture = new TextureLoader().load(woodImg);
+const groundTexture = new TextureLoader().load(grassImg);
+
+export {
+  dirtTexture,
+  logTexture,
+  grassTexture,
+  glassTexture,
+  woodTexture,
+  groundTexture,
+};
